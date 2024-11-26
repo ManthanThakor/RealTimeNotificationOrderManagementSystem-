8dd9f4eca7e67212cbf52c8007b2071ba52938f1
ManthanThakor <thakormanthan849@gmail.com>
2024-11-26 15:27:28 +0530

    Change

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RealTimeNotificationSystem
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Real-Time Notification System for Order Management Using Events and Delegates in C# \n");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
